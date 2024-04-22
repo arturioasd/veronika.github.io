8bdcc6a1bf6610a227508e5920f7d743980d3f35
arturioasd <132169879+arturioasd@users.noreply.github.com>
2024-04-23 02:38:17 +0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.header__burger').click(function(event){
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
+        $('.filter__mobile').toggleClass('delete-element');
     })
 })
 
@@ -9,6 +10,7 @@ $(document).ready(function(){
     $('.filter__mobile').click(function(event){
         $('.filter__title, .filter__body',).toggleClass('filter-swipe');
         $('.filter__mobile').toggleClass('filter-replace')
+        $('body').toggleClass('lock');
     })
 })
 
